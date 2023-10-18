1c01362fa7f5ccda22dce21d1647f558c7944c44
montillaleticia <montillaleticia@gmail.com>
2023-10-18 14:45:18 -0400

    Added documentation

--- main.r
+++ main.r
@@ -5,88 +5,53 @@ library(ggplot2)
 data <- airquality
 
 # Data Cleaning - Remove rows with NA
-data <- data[!is.na(data$Ozone) & !is.na(data$Solar.R) & !is.na(data$Wind) & !is.na(data$Temp) & !is.na(data$Month) & !is.na(data$Day), ]
-
-# Analysis Part 1: Calculate average Solar Radiation for each month
-avg_solar_May <- 0
-avg_solar_June <- 0
-avg_solar_July <- 0
-avg_solar_August <- 0
-avg_solar_September <- 0
-
-for (i in 1:nrow(data)) {
-    if (data$Month[i] == 5) {
-        avg_solar_May <- avg_solar_May + data$Solar.R[i]
-    } else if (data$Month[i] == 6) {
-        avg_solar_June <- avg_solar_June + data$Solar.R[i]
-    } else if (data$Month[i] == 7) {
-        avg_solar_July <- avg_solar_July + data$Solar.R[i]
-    } else if (data$Month[i] == 8) {
-        avg_solar_August <- avg_solar_August + data$Solar.R[i]
-    } else {
-        avg_solar_September <- avg_solar_September + data$Solar.R[i]
-    }
+clean_data <- function(data) {
+  data[complete.cases(data), ]
 }
 
-# Calculating average and print to console
-avg_solar_May <- avg_solar_May/31
-print(paste("Average Solar Radiation for May: ", avg_solar_May))
+data <- clean_data(data)
 
-avg_solar_June <- avg_solar_June/30
-print(paste("Average Solar Radiation for June: ", avg_solar_June))
+# Function to calculate the average Solar Radiation for a given month
+calculate_avg_solar <- function(data, month) {
+  avg_solar <- mean(data$Solar.R[data$Month == month])
+  return(avg_solar)
+}
 
-avg_solar_July <- avg_solar_July/31
-print(paste("Average Solar Radiation for July: ", avg_solar_July))
+# Function to calculate the correlation between Ozone and Solar Radiation for a given month
+calculate_correlation <- function(data, month) {
+  correlation <- cor(data$Ozone[data$Month == month], data$Solar.R[data$Month == month])
+  return(correlation)
+}
 
-avg_solar_August <- avg_solar_August/31
-print(paste("Average Solar Radiation for August: ", avg_solar_August))
+# Function to generate and save a plot
+generate_and_save_plot <- function(data, month, title, filename) {
+  plot <- ggplot(data[data$Month == month, ], aes(x = Solar.R, y = Ozone)) +
+    geom_point(aes(shape = factor(Month))) +
+    ggtitle(title)
+  ggsave(filename, plot)
+}
 
-avg_solar_September <- avg_solar_September/30
-print(paste("Average Solar Radiation for September: ", avg_solar_September))
+# Calculate average Solar Radiation and print to console
+months <- c(5, 6, 7, 8, 9)
 
-# Analysis Part 2: Correlation between Ozone and Solar Radiation for each month
-correlation_May <- cor(data$Ozone[data$Month == 5], data$Solar.R[data$Month == 5])
-print(paste("Correlation for May: ", correlation_May))
+for (month in months) {
+  avg_solar <- calculate_avg_solar(data, month)
+  cat(paste("Average Solar Radiation for ", month, ": ", avg_solar, "\n"))
+}
 
-correlation_June <- cor(data$Ozone[data$Month == 6], data$Solar.R[data$Month == 6])
-print(paste("Correlation for June: ", correlation_June))
-
-correlation_July <- cor(data$Ozone[data$Month == 7], data$Solar.R[data$Month == 7])
-print(paste("Correlation for July: ", correlation_July))
-
-correlation_August <- cor(data$Ozone[data$Month == 8], data$Solar.R[data$Month == 8])
-print(paste("Correlation for August: ", correlation_August))
-
-correlation_September <- cor(data$Ozone[data$Month == 9], data$Solar.R[data$Month == 9])
-print(paste("Correlation for September: ", correlation_September))
+# Calculate correlation and print to console
+for (month in months) {
+  correlation <- calculate_correlation(data, month)
+  cat(paste("Correlation for ", month, ": ", correlation, "\n"))
+}
 
 # Visualization
-plot_shapes <- vector("character", length=nrow(data))
-for (i in 1:nrow(data)) {
-    if (correlation_May > 0.5) {
-        plot_shapes[i] <- 19
-    } else if (correlation_June > 0.5) {
-        plot_shapes[i] <- 17
-    } else {
-        plot_shapes[i] <- 15
-    }
-}
+plot_shapes <- ifelse(correlation_May > 0.5, 19, ifelse(correlation_June > 0.5, 17, 15))
 
 # Save Plots
-g1 <- ggplot(data[data$Month == 5,], aes(x = Solar.R, y = Ozone)) + geom_point(aes(shape = factor(Month))) + ggtitle("May")
-ggsave("plot_may.png", g1)
-
-g2 <- ggplot(data[data$Month == 6,], aes(x = Solar.R, y = Ozone)) + geom_point(aes(shape = factor(Month))) + ggtitle("June")
-ggsave("plot_june.png", g2)
-
-g3 <- ggplot(data[data$Month == 7,], aes(x = Solar.R, y = Ozone)) + geom_point(aes(shape = factor(Month))) + ggtitle("July")
-ggsave("plot_july.png", g3)
-
-g4 <- ggplot(data[data$Month == 8,], aes(x = Solar.R, y = Ozone)) + geom_point(aes(shape = factor(Month))) + ggtitle("August")
-ggsave("plot_august.png", g4)
-
-g5 <- ggplot(data[data$Month == 9,], aes(x = Solar.R, y = Ozone)) + geom_point(aes(shape = factor(Month))) + ggtitle("September")
-ggsave("plot_september.png", g5)
+for (month in months) {
+  generate_and_save_plot(data, month, month.name[month], paste("plot_", tolower(month.name[month]), ".png", sep = ""))
+}
 
 # Save data
 write.csv(data, "cleaned_data.csv", row.names = FALSE)
